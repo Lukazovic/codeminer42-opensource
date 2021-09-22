ef7e1382c51285a9949245b4b47ab47c4b6efb8d
Lucas Vieira <lucas10.vieira.lv@gmail.com>
2021-09-22 15:38:04 -0300

    Improve Logo accessibility on focus

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,22 +15,24 @@ const Header = () => (
       justifyContent='space-between'
       height='60px'
     >
-      <A as={Flex} href='/' alignItems='center'>
-        <Logo />
+      <A href='/'>
+        <Flex alignItems='center'>
+          <Logo />
 
-        <Heading
-          as='h1'
-          ml='2'
-          fontFamily='logo'
-          fontSize={['6', '6', '7']}
-          fontWeight='600'
-          fontStyle='normal'
-        >
-          Code
-          <Text as='span' color='gray.600'>
-            Miner
-          </Text>
-        </Heading>
+          <Heading
+            as='h1'
+            ml='2'
+            fontFamily='logo'
+            fontSize={['6', '6', '7']}
+            fontWeight='600'
+            fontStyle='normal'
+          >
+            Code
+            <Text as='span' color='gray.600'>
+              Miner
+            </Text>
+          </Heading>
+        </Flex>
       </A>
 
       <Flex justifyContent='space-between' width='100px'>
