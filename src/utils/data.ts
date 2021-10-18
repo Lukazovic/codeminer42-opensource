eb2445eed99365bef37d6a944751d8001a119608
Lucas Vieira <lucas10.vieira.lv@gmail.com>
2021-10-18 19:28:24 -0300

    Add data file with current projects

---
+++ src/utils/data.ts
@@ -0,0 +1,33 @@
+export type Project = {
+  name: string
+  description: string
+  stars: number
+  forks: number
+  repository: string
+}
+
+export const projectsData: Project[] = [
+  {
+    name: 'Central',
+    description:
+      'Fork and Evolution of the Fulcrum project - An agile project planning tool and Pivotal Tracker drop-in replacement',
+    stars: 284,
+    forks: 59,
+    repository: 'https://github.com/Codeminer42/cm42-central',
+  },
+  {
+    name: 'Punchclock',
+    description:
+      'A simple electronic punch clock to track hours spent on projects',
+    stars: 16,
+    forks: 11,
+    repository: 'https://github.com/Codeminer42/Punchclock',
+  },
+  {
+    name: 'Marvin',
+    description: 'Marvin is a chat bot built on the Hubot framework',
+    stars: 6,
+    forks: 3,
+    repository: 'https://github.com/Codeminer42/marvin-cm42',
+  },
+]
